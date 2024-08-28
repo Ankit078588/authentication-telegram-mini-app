24b8b28d1d2364a2adb4479b328c84048e1d233e
Adarsh kumar <adarshkumar@Adarshs-MacBook-Pro.local>
2024-08-28 12:47:15 +0530

    updated

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post('/authenticate', async (req, res) => {
     try {
         const BOT_TOKEN = '7266000337:AAFxRzIlq8Q4I-3I2lAG-_PoffQehH-o0EM';
         const secret = crypto.createHash('sha256').update(BOT_TOKEN).digest().toString('hex');
-        const dataCheckString = `auth_date=${authDate}\nid=${telegramUserId}`;
+        const dataCheckString = `auth_date=${authDate}\nid=${telegramUserId}\nfirst_name=${name}`;
         const computedHash = crypto.createHmac('sha256', secret).update(dataCheckString).digest('hex');
         console.log('5: ' + BOT_TOKEN);
         console.log('6: ' + secret);
